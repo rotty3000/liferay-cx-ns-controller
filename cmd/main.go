ef693c98ee31710cc3f3a7b3d91d85d80be062f7
Raymond Augé <raymond.auge@liferay.com>
2025-05-26 10:40:34 -0400

    kubebuilder create api --group core --version v1 --kind ConfigMap --namespaced --resource=false --controller

--- cmd/main.go
+++ cmd/main.go
@@ -36,6 +36,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.com/liferay/liferay-portal/liferay-cx-ns-controller/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -198,6 +200,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := (&controller.ConfigMapReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ConfigMap")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
